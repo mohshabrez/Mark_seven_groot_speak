e3868b86497d7b7971ec24c9210f5dfd56357834
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2022-09-09 18:14:06 +0530

    Final Test --pirates speak

---
+++ app.js
@@ -0,0 +1,36 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+// outputDiv.innerText = "Shabrez"
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURL(input){
+   return serverURL + "?" + "text=" + input
+
+}
+
+function errorHandler(error){
+   console.log("error occured", error);
+   alert("something wrong with server: try again after some time");
+}
+
+function clickHandler(){
+   // outputDiv.innerText = "wdheduwvwjfryf" + txtInput.value;
+   var inputText = txtInput.value;
+
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;})
+
+      // console.log(json.contents.translated))
+   .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
+// eloquentjavascript.net
